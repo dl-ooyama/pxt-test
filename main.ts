21e8bd14194052f38704152cc4b5adf129bd32ba
Aya Shirasaki <14908109+dl-shirasaki@users.noreply.github.com>
2018-01-25 23:14:09 +0900

    Test

--- main.ts
+++ main.ts
@@ -1,7 +1,53 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+
+/**
+ * このファイルを使って、独自の関数やブロックを定義してください。
+ * 詳しくはこちらを参照してください：https://makecode.microbit.org/blocks/custom
+ */
+
+enum Switch {
+    //% block="スイッチ 1"
+    One,
+    //% block="スイッチ 2"
+    Two
+}
+
+/**
+ * カスタムブロック
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace mytest {
+    /**
+     * サーボを回転させて通電させます。
+     * @param s どちらのスイッチを ON にするか
+     */
+    //% blockId="switch_on"
+    //% block="%s|を ON にする"
+    export function switchOn(s: Switch): void {
+        switch (s) {
+            case Switch.One:
+                pins.servoWritePin(AnalogPin.P2, 0);
+                break;
+            case Switch.Two:
+                pins.servoWritePin(AnalogPin.P1, 0);
+                break;
+        }
+    }
+
+    /**
+     * サーボを 0 度にして通電しないようにします。
+     * @param s どちらのスイッチを OFF にするか
+     */
+    //% blockId="switch_off"
+    //% block="%s|を OFF にする"
+    export function switchOff(s: Switch): void {
+        switch (s) {
+            case Switch.One:
+                pins.servoWritePin(AnalogPin.P2, 90);
+                break;
+            case Switch.Two:
+                pins.servoWritePin(AnalogPin.P1, 90);
+                break;
+        }
+    }
+}
+
--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
 // tests go here; this will not be compiled when this package is used as a library
+
+mytest.switchOn(Switch.One)
+basic.pause(2000)
+mytest.switchOff(Switch.One)
+basic.pause(2000)
+
+
